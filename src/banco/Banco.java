a0a208300f2b5ca64ac73ac00783ee8c9810c579
Hannatamayo1 <hanna.tamayo@mariaelvinia.edu.co>
2019-09-25 12:59:19 -0500

    versión 1.0
     Agregamos atributos de clase

---
+++ src/banco/Banco.java
@@ -0,0 +1,19 @@
+
+package banco;
+
+import java.util.Date;
+
+
+
+
+public class Banco {
+    
+private int numeroDeCajas;
+private int numeroDeTrabajadores;
+private Date fechaDeApertura;
+private String nombreDelDueno;
+private boolean tieneSucursales;
+         
+
+
+        }
