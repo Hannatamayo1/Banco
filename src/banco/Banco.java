2b4f3dd2f55d7889162ca35c68be4e215a0eb828
Hannatamayo1 <51164668+Hannatamayo1@users.noreply.github.com>
2019-10-02 13:04:01 -0500

    version 1.2

--- src/banco/Banco.java
+++ src/banco/Banco.java
@@ -1,19 +1,62 @@
-
 package banco;
 
 import java.util.Date;
 
-
-
-
 public class Banco {
+
+    private int numeroDeCajas;
+    private int numeroDeTrabajadores;
+    private Date fechaDeApertura;
+    private String nombreDelDueno;
+    private boolean tieneSucursales;
+
+    public Banco(int numeroDeCajas, int numeroDeTrabajadores, Date fechaDeApertura, String nombreDelDueno, boolean tieneSucursales) {
+        this.numeroDeCajas = numeroDeCajas;
+        this.numeroDeTrabajadores = numeroDeTrabajadores;
+        this.fechaDeApertura = fechaDeApertura;
+        this.nombreDelDueno = nombreDelDueno;
+        this.tieneSucursales = tieneSucursales;
+    }
+
+    public int getNumeroDeCajas() {
+        return numeroDeCajas;
+    }
+
+    public void setNumeroDeCajas(int numeroDeCajas) {
+        this.numeroDeCajas = numeroDeCajas;
+    }
+
+    public int getNumeroDeTrabajadores() {
+        return numeroDeTrabajadores;
+    }
+
+    public void setNumeroDeTrabajadores(int numeroDeTrabajadores) {
+        this.numeroDeTrabajadores = numeroDeTrabajadores;
+    }
+
+    public Date getFechaDeApertura() {
+        return fechaDeApertura;
+    }
+
+    public void setFechaDeApertura(Date fechaDeApertura) {
+        this.fechaDeApertura = fechaDeApertura;
+    }
+
+    public String getNombreDelDueno() {
+        return nombreDelDueno;
+    }
+
+    public void setNombreDelDueno(String nombreDelDueno) {
+        this.nombreDelDueno = nombreDelDueno;
+    }
+
+    public boolean isTieneSucursales() {
+        return tieneSucursales;
+    }
+
+    public void setTieneSucursales(boolean tieneSucursales) {
+        this.tieneSucursales = tieneSucursales;
+    }
+
     
-private int numeroDeCajas;
-private int numeroDeTrabajadores;
-private Date fechaDeApertura;
-private String nombreDelDueno;
-private boolean tieneSucursales;
-         
-
-
-        }
+}
